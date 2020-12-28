e7110c2c153c2e37be0f4e25c2ffa0a0fc1b6fa5
WeraGit <weronikalimberger@gmail.com>
2020-12-28 10:45:02 +0000

    final

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $(function(){
+
     var fishes;
+    
     fetch('https://api.fourteenfish.com/api/v2/developers/coding-task-data')
         .then(function (response) {
           return response.json();
@@ -7,12 +9,12 @@ $(function(){
         .then(function (data) {
           // todo: append data to the page
           console.log(data);
-          //trying to return the data:
+          //test: trying to return the data:
           // alert(data[1].id);
 
           fishes = data;
 
-          //trying to insert data to the fish name section
+          //test: trying to insert data to the fish name section
           // $('#fish-name').html(fishes[0].name);                  
 
         }).catch(function (err) {
@@ -28,25 +30,38 @@ $(function(){
 
     $('#select-fish').on('change', function(){
         // todo: display relevant data
-        //check if the function 'on-change' works:
+        //test: check if the function 'on-change' works:
         // alert($('#select-fish').val());
-        //below was used to check/figure out how to pull the data/ and if the data is available globally:
+        //test: below was used to check/figure out how to pull the data/ and if the data is available globally:
         // $('#fish-name').html(fishes[4].name);
       for (i=0; i<fishes.length; i++) {
-        //checking if the loop works and pulles the right values from the 'fishes':
+        //test: checking if the loop works and pulles the right values from the 'fishes':
         // alert(fishes[i].name); 
         if ($('#select-fish').val() == fishes[i].id) {
-          //below 'alert' is used to check if the 'if statement' works as expected
+          //test: below 'alert' is used to check if the 'if statement' works as expected
           // alert('Hello');
-          $('#fish-name').html('Nice, you have chosen:  ' + capitalise(fishes[i].name));
-          $('.fish-description').html(fishes[i].description);
+          $('#fish-name').html(capitalise(fishes[i].name));
+          $('.fish-description').html('Good choice! The ' + fishes[i].description);
           //cod has no src provided, below should ensure that there is a picture loaded on the page anyway
           if (fishes[i].id == 198 && fishes[i].image == ""){
-            $('.fish-image').attr('src', 'https://youngsseafood.co.uk/wp-content/uploads/2017/06/atlantic-cod-300x115.png');
+            $('.fish-image').attr('src', 'https://youngsseafood.co.uk/wp-content/uploads/2017/06/atlantic-cod-612x235.png');
           } else {
             $('.fish-image').attr('src', fishes[i].image);
+          }      
+          //found a picuture of the seabass with transparent background
+          if (fishes[i].id == 132 && fishes[i].image == "https://cdn.britannica.com/43/9443-050-5D22A052/Black-sea-bass.jpg"){
+            $('.fish-image').attr('src', 'https://youngsseafood.co.uk/wp-content/uploads/2017/06/sea-bass-612x239.png')
           }
-          
+          //swapping to an image of mackerel with transparent background
+          if (fishes[i].id == 157 && fishes[i].image == "https://s3.amazonaws.com/divcomplatform/seafoodsource.com/images/0c473c32fe2b29821e2153b89e8de385.jpeg"){
+            $('.fish-image').attr('src', 'https://youngsseafood.co.uk/wp-content/uploads/2017/06/Mackerel.png')
+          }
+          //herrings picture is tall, to move it up I have used negative value in the top margin 
+          if (fishes[i].id == 170 && fishes[i].image == "https://cdn2.webdamdb.com/1280_guAqnssccMN6.png?1523906515") {
+            $('.fish-image').css('margin-top', '-70px');
+          } else {
+            $('.fish-image').css('margin-top', '10px');
+          }
         }
       }
       //changing the wording of the fish-selector label
